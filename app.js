45c2ebfd6f4e328221eea401e4566e7afc80c1a2
Elias Lawson-Fox <eliaslfox@gmail.com>
2015-10-18 16:05:28 -0700

    Added Bower

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var passportRoute = require('./routes/passport');
 var yelpRoute = require("./routes/yelp");
-var foodRoute = require("./routes/food")
+var foodRoute = require("./routes/food");
 
 // passport config
 var Account = require('./models/account');
